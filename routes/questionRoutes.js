3b7c2e21ca3b6eb48b64513e31e1a891bc943485
Tata Anand Kishore <anandkishoretata@gmail.com>
2026-08-03 19:31:24 +0530

    backend setup

---
+++ routes/questionRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import {
+  getQuestions,
+} from "../controllers/questionController.js";
+
+const router =
+  express.Router();
+
+router.get(
+  "/",
+  getQuestions
+);
+
+export default router;
