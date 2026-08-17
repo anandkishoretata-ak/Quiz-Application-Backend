eb3820135aa96b059f95d51bd42247ae17d4660b
Tata Anand Kishore <anandkishoretata@gmail.com>
2026-08-17 19:03:15 +0530

    modify code

--- models/User.js
+++ models/User.js
@@ -5,17 +5,25 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true,
     },
 
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
     },
 
     password: {
       type: String,
       required: true,
+    },
+
+    role: {
+      type: String,
+      default: "student",
     },
   },
   {
